065151a0bbc5783e53d6adb8a22758355042a806
Arne Neumann <github@arne.cl>
2026-09-17 12:37:00 +0200

    fix: list input/output formats in CLI help and errors
    Constrain input_format and output_format with argparse choices so --help
    and invalid values show the supported formats, and use a custom parser
    error that also lists them when a required argument is missing. This
    replaces the uncaught KeyError traceback for unknown formats with a
    clean non-zero exit.

--- src/rstconverter/cli.py
+++ src/rstconverter/cli.py
@@ -5,11 +5,29 @@ import sys
 from .app import READ_FUNCTIONS, WRITE_FUNCTIONS
 
 
+class FormatParser(argparse.ArgumentParser):
+    """An ArgumentParser that reports the supported RST formats on error."""
+
+    def error(self, message):
+        self.print_usage(sys.stderr)
+        self.exit(2, "{prog}: error: {message}\n{formats}\n".format(
+            prog=self.prog, message=message, formats=self.format_list()))
+
+    @staticmethod
+    def format_list():
+        return ("Available input formats: {inputs}\n"
+                "Available output formats: {outputs}").format(
+                    inputs=', '.join(sorted(READ_FUNCTIONS)),
+                    outputs=', '.join(sorted(WRITE_FUNCTIONS)))
+
+
 def main():
-    parser = argparse.ArgumentParser()
+    parser = FormatParser()
     parser.add_argument('input_file', help='file to be converted')
-    parser.add_argument('input_format', help='format of the input file')
-    parser.add_argument('output_format', help='format of the output file')
+    parser.add_argument('input_format', choices=sorted(READ_FUNCTIONS),
+                        help='format of the input file')
+    parser.add_argument('output_format', choices=sorted(WRITE_FUNCTIONS),
+                        help='format of the output file')
     
     parser.add_argument('output_file', nargs='?', default=None)
     
@@ -22,14 +40,8 @@ def main():
     except Exception as ex:
         logging.exception("Can't handle input file {}".format(args.input_file))
         sys.exit(1)
-    
-    try:
-        write_function = WRITE_FUNCTIONS[args.output_format]
-    except KeyError:
-        raise KeyError(
-            f"Wrong output format '{args.output_format}'. "
-            f"Available formats: {list(WRITE_FUNCTIONS.keys())}"
-        )
+
+    write_function = WRITE_FUNCTIONS[args.output_format]
     
     output_file = args.output_file if args.output_file is not None else sys.stdout
 
